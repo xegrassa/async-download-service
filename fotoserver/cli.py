363e7d0169754cb7ad4704a2647f864df3b8c95f
Egrassa <bacek90@mail.ru>
2021-06-16 22:22:05 +0500

    Remove env

--- fotoserver/cli.py
+++ fotoserver/cli.py
@@ -1,11 +1,12 @@
 import argparse
-
+import os
 
 def parse_arguments():
+    default_directory = os.path.join(os.getcwd(), 'test_photos')
     parser = argparse.ArgumentParser(description='Сервер скачивания фотоархива')
     parser.add_argument('-d', '--delay', type=int, default=0,
                         help='Задержка перед отправкой Чанка в секундах')
-    parser.add_argument('-dir', '--directory', default=None,
+    parser.add_argument('-dir', '--directory', default=default_directory,
                         help='Путь до директории с фото')
     parser.add_argument('--no-logging', action='store_true',
                         help='Отключить Логгирование по дефолту включено')
--- server.py
+++ server.py
@@ -1,58 +1,42 @@
 import asyncio
-import datetime
 import os
 
 import aiofiles
 from aiohttp import web
-from dotenv import load_dotenv
 
 from fotoserver.cli import parse_arguments
 from fotoserver.logging import configure_logging
 
 
-async def uptime_handler(request):
-    delay = request.app['delay']
-    response = web.StreamResponse()
-    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
-    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
-    response.headers['Content-Type'] = 'text/html'
-
-    # Отправляет клиенту HTTP заголовки
-    await response.prepare(request)
-    while True:
-        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
-        # Отправляет клиенту очередную порцию ответа
-        await response.write(message.encode('utf-8'))
-        await asyncio.sleep(delay)
-
-
 async def archivate(request):
-    directory = request.app['directory']
+    root_photo_directory = request.app['directory']
     delay = request.app['delay']
-    hash = request.match_info['archive_hash']
-    file_path = os.path.join(directory, hash)
-    if not os.path.exists(file_path):
+    archive_hash = request.match_info['archive_hash']
+    dir_for_archiving = os.path.join(root_photo_directory, archive_hash)
+    if not os.path.exists(dir_for_archiving):
         raise web.HTTPNotFound(reason='Архив не существует или был удален')
 
+    headers = {
+        'Content-Type': 'text/html',
+        'Transfer-Encoding': 'chunked',
+        'Content-Disposition': f'attachment; filename="{archive_hash}.rar"'
+    }
     response = web.StreamResponse()
-    response.headers['Content-Type'] = 'text/html'
-    response.headers['Transfer-Encoding'] = 'chunked'
-    response.headers['Content-Disposition'] = f'attachment; filename="{hash}.rar"'
+    response.headers.update(headers)
     await response.prepare(request)
 
     program = 'zip'
-    params = ('-qr', '-', f'{file_path}')
+    params = ('-qr', '-', f'{dir_for_archiving}')
     process = await asyncio.create_subprocess_exec(program, *params, stdout=asyncio.subprocess.PIPE)
 
     try:
         while not process.stdout.at_eof():
-            logger.debug(f'Sending archive chunk {hash}')
+            logger.debug(f'Sending archive chunk {archive_hash}')
             chunk = await process.stdout.read(500000)  # 500Kb
             await response.write(chunk)
             await asyncio.sleep(delay)
     except (asyncio.CancelledError, BaseException):
-        logger.debug(f'Download was interrupted {hash}')
+        logger.debug(f'Download was interrupted {archive_hash}')
         process.terminate()
         logger.info('zip process was killed')
         raise
@@ -66,17 +50,14 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    default_directory = os.getenv('DIRECTORY')
     args = parse_arguments()
     logger = configure_logging(args.no_logging)
 
     app = web.Application()
     app['delay'] = args.delay
-    app['directory'] = args.directory if args.directory else default_directory
+    app['directory'] = args.directory
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archivate),
-        web.get('/test', uptime_handler),
     ])
     web.run_app(app)
